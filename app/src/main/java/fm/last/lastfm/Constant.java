e118ca6981be37cb251d8406ac642da0ba2b0929
Gaffar Akhtar <gaffar.akthar@gmail.com>
2019-01-03 19:10:20 +0530

    added missing first character from API key]

--- app/src/main/java/fm/last/lastfm/Constant.java
+++ app/src/main/java/fm/last/lastfm/Constant.java
@@ -3,5 +3,5 @@ package fm.last.lastfm;
 public class Constant {
     public static final String KEY_ALBUM = "album";
     public static final String BASE_URL = "http://ws.audioscrobbler.com";
-    public static final String API_KEY ="867e7978cca6391a71a5d325fa7f93c";
+    public static final String API_KEY ="6867e7978cca6391a71a5d325fa7f93c";
 }
